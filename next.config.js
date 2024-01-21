37a8d7468410435c97475f44f5890748ce1cd14c
kamalbuilds <geniusamansingh@gmail.com>
2024-01-21 05:51:24 +0530

    tryin?

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
 	webpack: config => {
 		config.resolve.fallback = { fs: false, net: false, tls: false }
+
+		if (isServer) {
+			config.plugins.push(
+			  new webpack.DefinePlugin({
+				'window': '{}',
+			  })
+			);
+		  }
 		return config
 	},
 }

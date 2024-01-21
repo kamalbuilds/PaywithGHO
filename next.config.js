d68f52de957fa66608168ea49deb125fd0a4db6e
kamalbuilds <geniusamansingh@gmail.com>
2024-01-21 06:11:21 +0530

    reverting back

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
 	webpack: config => {
 		config.resolve.fallback = { fs: false, net: false, tls: false }
-
-		if (isServer) {
-			config.plugins.push(
-			  new webpack.DefinePlugin({
-				'window': '{}',
-			  })
-			);
-		  }
 		return config
 	},
 }
